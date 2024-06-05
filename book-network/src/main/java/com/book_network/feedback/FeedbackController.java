b4e389e59e5fe7dccf895c5c586e058aeb50b4c7
T Pavan Kalyan <tpavankalyan77@gmail.com>
2024-06-05 21:19:54 +0530

    Added Openapi to the angular as a JSON formate and also add UI dependencies Bootstrap and fortawesome for anguler using this dependents make beatifullIntegrate OpenAPI with JSON format, add Bootstrap and Fortawesome for UI enhancements, and set up Swagger UI- Added OpenAPI specification in JSON format for API documentation.- Installed and configured Swagger UI to visualize and interact with the API.- Integrated Bootstrap for responsive and modern UI components.- Added Font Awesome for scalable vector icons.- Enhanced overall UI aesthetics using the newly added dependencies.- Updated relevant Angular components and services to align with the OpenAPI spec.- Ensured compatibility and smooth integration between OpenAPI, Swagger UI, Bootstrap, and Font Awesome.

--- book-network/src/main/java/com/book_network/feedback/FeedbackController.java
+++ book-network/src/main/java/com/book_network/feedback/FeedbackController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +17,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("feedbacks")
+@RequestMapping("/feedbacks")
 @RequiredArgsConstructor
 @Tag(name = "Feedback")
 public class FeedbackController {
 
 	private final FeedbackService service;
 
+	@PostMapping
 	public ResponseEntity<Integer> saveFeedback(@Valid @RequestBody FeedbackRequest reqest,
 			Authentication connectedUser) {
 
